8153a20ac15574e88308f8d30f3657ee33e3ff30
Niko Aurelma <naurelma@gmail.com>
2019-06-12 09:12:32 +0300

    ennen

--- kaikki.bash
+++ kaikki.bash
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+if [[ ! -n "$1" ]] ; then
+	echo "Usage: $0 targetDir"
+	exit
+fi
+base=$PWD
 
+cd "$1"
 unzip OneDrive*
 7z x Consumption_data.7z -o"Consumption_data" -aos
 7z x Temperature.7z -o"Temperature" -aos
 
-./script/siivoa.bash
-./script/jaaxlsx.bash
-./script/haetmp.bash
+$base/siivoa.bash
+$base/jaaxlsx.bash
+$base/haetmp.bash
