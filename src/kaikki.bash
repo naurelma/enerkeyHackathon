a11716830142b4536be6d631fb4a6f9c28b404c1
Niko Aurelma <naurelma@gmail.com>
2019-06-13 12:01:27 +0300

    siirretty

---
+++ src/kaikki.bash
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [[ ! -n "$1" ]] ; then
+	echo "Usage: $0 targetDir"
+	exit
+fi
+SCRIPTIT=$PWD
+export SCRIPTIT
+
+cd "$1"
+unzip OneDrive*
+7z x Consumption_data.7z -o"Consumption_data" -aos
+7z x Temperature.7z -o"Temperature" -aos
+
+$SCRIPTIT/siivoa.bash
+$SCRIPTIT/jaaxlsx.bash
+$SCRIPTIT/haetmp.bash
+$SCRIPTIT/merge.bash
